31e8f46d62aecaa3c20724206f769c245486a42c
BlueSkyXN <xienan0714@gmail.com>
2021-04-13 19:32:25 +0800

    Create yum-git-jsd.sh

---
+++ yum-git-jsd.sh
@@ -0,0 +1,9 @@
+sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel asciidoc gcc perl-ExtUtils-MakeMaker
+sudo yum -y update
+sudo yum -y remove git
+wget https://cdn.jsdelivr.net/gh/BlueSkyXN/Yum-Git/git-2.31.1.tar.gz
+tar -zvxf git-2.31.1.tar.gz
+cd git-2.31.1
+sudo make prefix=/usr/local/git all
+sudo make prefix=/usr/local/git install
+sudo ln -s /usr/local/git/bin/git /usr/bin/git
