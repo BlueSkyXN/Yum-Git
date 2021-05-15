2e5d40ee94513e8c51a983daf9ba5198302d6e92
BlueSkyXN <xienan0714@gmail.com>
2021-05-15 19:24:33 +0800

    Update yum-git-raw.sh

--- yum-git-raw.sh
+++ yum-git-raw.sh
@@ -1,7 +1,7 @@
 sudo yum -y install curl-devel expat-devel gettext-devel openssl-devel zlib-devel asciidoc gcc perl-ExtUtils-MakeMaker
 sudo yum -y update
 sudo yum -y remove git
-wget https://raw.githubusercontent.com/BlueSkyXN/Yum-Git/main/git-2.31.1.tar.xz
+wget https://raw.githubusercontent.com/BlueSkyXN/Yum-Git/main/git-2.31.1.tar.gz
 tar -zvxf git-2.31.1.tar.gz
 cd git-2.31.1
 sudo make prefix=/usr/local/git all
